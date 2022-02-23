fb5b1c384848abf83d10b8efca8ae2022059d434
nameuser01 <nomail@thisisnotamailservice.noone>
2022-02-23 18:09:07 +0100

    Update code

--- sorter.py
+++ sorter.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python3
+import time
 import matplotlib.pyplot as plt
 from collections import Counter
-import stats_random as func
-# import file_values
+import random
+import string
+import re
+import sys
 
-#	Way to use the generate function #
-# generate(<start teminal>, <stop terminal>, <number of entities to generate>)
+# Control
+ACCURACY = 10
 
-data = []
-data = func.generate(1, 150, 10000000)
-stats = Counter(data).most_common()
+file_reading = open(sys.argv[1], "r")
+file_content = file_reading.readlines()
+file_reading.close()
 
-number_id = []
-number_rep = []
-i = 0
-for i in range (len(stats)):
-	number_id.append(stats[i][0])
-	number_rep.append(stats[i][1])
-print(number_id)
-print(number_rep)
-plt.bar(number_id, number_rep, color="blue")
-plt.grid(True)
+liens = []
+
+for line in file_content:
+	if(line[0:5] == "https"):
+		next_stop = line.find('/', 9)
+		liens.append(line[8:next_stop])
+	elif(line[0:5] == "http:"):
+		next_stop = line.find('/', 8)
+		liens.append(line[7:next_stop])
+	else:
+		pass
+	# print(line.strip())
+	# time.sleep(0.2)
+
+cnt = Counter()
+for domaine in liens:
+	cnt[domaine] += 1
+
+# Récupération des éléments qui se répètent le plus
+domain_name = []
+domain_rep = []
+for k, v in sorted(cnt.items(), key=lambda item: item[1]):
+	if (v >= ACCURACY):
+		domain_name.append(k)
+		domain_rep.append(v)
+	else:
+		pass
+
+plt.barh(domain_name, domain_rep)
+plt.ylabel("Nombre d'occurences")
 plt.show()
-
-print(stats)
